f66d82464a7328ee46367d713016a0e76206189d
김도영 <rlaehdud1002@naver.com>
2024-09-09 22:35:49 +0900

    [FEAT] 카카오 로그인 구현

--- client/src/page/login/components/KakaoLogin.tsx
+++ client/src/page/login/components/KakaoLogin.tsx
@@ -1,10 +1,18 @@
+import { KAKAO_AUTH_URL } from "api/clientApi";
+
 import { RiKakaoTalkFill } from "react-icons/ri";
 const KakaoLogin = () => {
   return (
-    <div className="flex flex-row items-center bg-[#FEE500] px-4 py-2 rounded-xl">
+    <button
+      className="flex flex-row items-center bg-[#FEE500] px-4 py-2 rounded-xl"
+      onClick={() => {
+        window.location.href = KAKAO_AUTH_URL;
+      }}
+      type="button"
+    >
       <RiKakaoTalkFill size={32} className="mr-2" color="#3B1E1E" />
       <span className="font-bold mx-auto">카카오로 시작하기</span>
-    </div>
+    </button>
   );
 };
 
