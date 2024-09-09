233a86fd37fd50d1f8ed4449fc669ba38713ea31
Seo Minsu <seominsu@Soo-MacBookPro.local>
2024-09-10 00:45:42 +0900

    [FEAT] 설문조사 페이지 UI

--- client/src/page/login/components/Gender.tsx
+++ client/src/page/login/components/Gender.tsx
@@ -21,13 +21,13 @@ const Gender = () => {
         <span>더 좋은 책과 문장을 추천드리기 위해 활용됩니다.</span>
       </div>
 
-      <div className='grid grid-cols-2 gap-14 mt-28'>
+      <div className='grid grid-cols-2 gap-14 mt-28 w-full max-w-sm'>
         <div className='flex flex-col items-center gap-4 '>
           <button
             className={getButtonClass('male')}
             onClick={() => handleSelectGender('male')}
           >
-            <img src='/images/survey/male.png' alt='male' />
+            <img src='/images/survey/male.png' alt='male'/>
           </button>
           <span className='text-xl font-semibold'>남성</span>
         </div>
--- client/src/page/login/page/SurveyPage.tsx
+++ client/src/page/login/page/SurveyPage.tsx
@@ -2,17 +2,20 @@ import { IoIosArrowBack } from 'react-icons/io';
 import Consent from '../components/Consent';
 import Age from '../components/Age';
 import Gender from '../components/Gender';
+import Nickname from '../components/Nickname';
+import Interest from '../components/Interest';
 const SurveyPage = () => {
   return (
-    <div className='flex flex-col px-4 h-screen max-h-[750px]'>
+    <div className='flex flex-col px-4 h-screen '>
       <button className='pt-7'>
         <IoIosArrowBack className='text-4xl' />
       </button>
       {/* <Consent /> */}
       {/* <Age /> */}
-      <Gender />
-
-      <div className='mt-auto'>
+      {/* <Gender /> */}
+      {/* <Nickname/> */}
+      {/* <Interest /> */}
+      <div className='mt-auto w-full mb-32'>
         <button className='bg-[#776B5D] font-bold w-full text-lg text-white py-3 rounded-lg'>
           다음
         </button>
