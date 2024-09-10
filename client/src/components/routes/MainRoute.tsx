441d7af5d737b80be10a3ff9d8428c136cf3c8a3
김도영 <rlaehdud1002@naver.com>
2024-09-10 11:45:52 +0900

    [FEAT] 대여 도서 상세 정보 페이지 ui

--- client/src/components/routes/MainRoute.tsx
+++ client/src/components/routes/MainRoute.tsx
@@ -3,22 +3,23 @@ import ChattingPage from "page/chatting/page/ChattingPage";
 import KaPayApprovePage from "page/kapay/page/KaPayApprovePage";
 import KaPayCancelPage from "page/kapay/page/KaPayCancelPage";
 import KaPayPage from "page/kapay/page/KaPayPage";
+import BookDetailPage from "page/library/page/BookDetailPage";
 import CartPage from "page/library/page/CartPage";
 import LibraryPage from "page/library/page/LibraryPage";
 import MyFavoritePage from "page/library/page/MyFavoritePage";
+import RentalBookDetailPage from "page/library/page/RentalBookDetailPage";
 import LocationPage from "page/location/page/LocationPage";
 import LocationSettingPage from "page/location/page/LocationSettingPage";
+import AuthCallBack from "page/login/page/AuthCallbackPage";
 import LoginPage from "page/login/page/LoginPage";
+import SurveyPage from "page/login/page/SurveyPage";
 import MainPage from "page/main/page/MainPage";
-import ProfilePage from "page/profile/page/ProfilePage";
 import GradePage from "page/profile/page/GradePage";
 import LeafPage from "page/profile/page/LeafPage";
+import ProfilePage from "page/profile/page/ProfilePage";
 import SearchPage from "page/search/page/SearchPage";
 import SearchResultPage from "page/search/page/SearchResultPage";
 import { Route, Routes } from "react-router-dom";
-import BookDetailPage from "page/library/page/BookDetailPage";
-import SurveyPage from 'page/login/page/SurveyPage';
-import AuthCallBack from "page/login/page/AuthCallbackPage";
 
 const MainRoute = () => {
   return (
@@ -37,13 +38,14 @@ const MainRoute = () => {
       <Route path="/cart" element={<CartPage />} />
       <Route path="/alarm" element={<AlarmPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/auth/callback" element={<AuthCallBack />}/>
+      <Route path="/auth/callback" element={<AuthCallBack />} />
       <Route path="/kapay" element={<KaPayPage />} />
       <Route path="/kapay/approve" element={<KaPayApprovePage />} />
       <Route path="/kapay/cancel" element={<KaPayCancelPage />} />
       <Route path="/kapay/fail" element={<KaPayCancelPage />} />
       <Route path="/bookdetail" element={<BookDetailPage />} />
-      <Route path='/survey' element={<SurveyPage />} />
+      <Route path="/bookdetail/rental" element={<RentalBookDetailPage />} />
+      <Route path="/survey" element={<SurveyPage />} />
     </Routes>
   );
 };
