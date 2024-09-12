3915fe230fd3cb4115ce3ee522920da6d926f965
김도영 <rlaehdud1002@naver.com>
2024-09-12 17:18:02 +0900

    [FEAT] 채팅 리스트

--- client/src/page/chatting/page/ChattingPage.tsx
+++ client/src/page/chatting/page/ChattingPage.tsx
@@ -1,7 +1,24 @@
+import ChatList from "page/chatting/components/ChatList";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+
 const ChattingPage = () => {
   return (
-    <div>
-      <h1>Chatting</h1>
+    <div className="mx-[25px]">
+      <div className="flex flex-row justify-between my-4">
+        <span className="font-bold text-2xl">채팅</span>
+        <Link to="/alarm">
+          <IoNotificationsOutline size={27} color="#545454" />
+        </Link>
+      </div>
+      <div>
+        <ChatList />
+        <ChatList />
+        <ChatList />
+        <ChatList />
+        <ChatList />
+        <ChatList />
+      </div>
     </div>
   );
 };
