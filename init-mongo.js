61ed096a7d678392cbb9139e36270f8086fbc1e4
lij0825 <dldlswns890@gmail.com>
2024-09-14 16:05:47 +0900

    [FIX] init - mongo 수정

--- init-mongo.js
+++ init-mongo.js
@@ -1,7 +1,18 @@
-// init-mongo.js
+// Switch to the admin database to create a root user
 db = db.getSiblingDB("admin");
+db.createUser({
+  user: "najackdo",
+  pwd: "najackdo",
+  roles: [{ role: "root", db: "admin" }],
+});
+
+// Authenticate as the newly created user
 db.auth("najackdo", "najackdo");
+
+// Switch to the database where you want to create users
 db = db.getSiblingDB("najackdo");
+
+// Create a user with readWrite role for the 'najackdo' database
 db.createUser({
   user: "najackdo",
   pwd: "najackdo",
