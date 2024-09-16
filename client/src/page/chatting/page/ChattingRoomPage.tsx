5a02bf8b28232b3b7114e30ee4bcf0862baaeda7
김도영 <rlaehdud1002@naver.com>
2024-09-16 13:38:21 +0900

    [FEAT] 컴포넌트 분리

--- client/src/page/chatting/page/ChattingRoomPage.tsx
+++ client/src/page/chatting/page/ChattingRoomPage.tsx
@@ -1,9 +1,10 @@
 import { Client } from "@stomp/stompjs";
 import { Input } from "components/ui/input";
+import ChatBookInfo from "page/chatting/components/ChatBookInfo";
 import ReceiveMessage from "page/chatting/components/ReceiveMessage";
 import SendMessage from "page/chatting/components/SendMessage";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
-import { IoIosLeaf, IoIosSend } from "react-icons/io";
+import { IoIosSend } from "react-icons/io";
 import { IoChevronBack } from "react-icons/io5";
 import { useLocation, useNavigate } from "react-router-dom";
 
@@ -128,26 +129,8 @@ const ChattingRoomPage = () => {
         <div />
       </div>
       {/* 채팅방 책 정보 */}
-      <div className="bg-[#DBD6D3] w-full h-24 px-4 flex flex-row items-center justify-between">
-        <div className="flex flex-row items-center">
-          <img
-            src={book.imageUrl}
-            alt="사진"
-            className="rounded-2xl w-16 h-16"
-          />
-          <div className="ml-2">
-            <span>{book.title}</span>
-            <div className="flex flex-row items-center">
-              <span className="text-black/50 text-sm">일일</span>
-              <span className="ml-2 mr-1 font-bold">{book.dayPrice}</span>
-              <IoIosLeaf color="#79AC78" size={20} />
-            </div>
-          </div>
-        </div>
-        <span className="bg-[#776B5D] text-white rounded-lg py-2 px-3">
-          약속 잡기
-        </span>
-      </div>
+      <ChatBookInfo book={book} />
+      {/* 채팅창 */}
       <div className="mx-[25px]">
         <div
           className="flex-grow overflow-y-auto py-4"
