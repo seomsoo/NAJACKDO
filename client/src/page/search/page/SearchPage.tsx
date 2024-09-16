c3e596ad679c7f934900e47acf99437bb8a9d230
김도영 <rlaehdud1002@naver.com>
2024-09-16 15:49:56 +0900

    [FEAT] 텍스트로 등록 추가, 추천 검색어 삭제

--- client/src/page/search/page/SearchPage.tsx
+++ client/src/page/search/page/SearchPage.tsx
@@ -1,7 +1,6 @@
 import { Input } from "components/ui/input";
 import RecentSearch from "page/search/components/RecentSearch";
 import RecommendBook from "page/search/components/RecommendBook";
-import RecommendSearch from "page/search/components/RecommendSearch";
 import { IoIosArrowBack, IoIosSearch } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 
@@ -14,16 +13,22 @@ const SearchPage = () => {
   return (
     <div className="mx-4 mt-8">
       <div className="flex flex-row items-center relative">
-        <IoIosArrowBack size={25} color="#545454" className="mr-2" />
+        <IoIosArrowBack
+          size={25}
+          color="#545454"
+          className="mr-2 cursor-pointer"
+        />
         <Input
           className="bg-[#D9D9D9] border-none"
           placeholder="검색어를 입력해주세요."
         />
-        <div className="absolute right-2" onClick={goSearchResult}>
+        <div
+          className="absolute right-2 cursor-pointer"
+          onClick={goSearchResult}
+        >
           <IoIosSearch size={25} color="#545454" />
         </div>
       </div>
-      <RecommendSearch />
       <RecentSearch />
       <RecommendBook />
     </div>
