4e16387fddd6808f310728e6800c9e129f2b00d7
이인준 <dldlswns890@naver.com>
2024-09-19 14:51:20 +0900

    [Merge] 병합 충돌 해결

--- server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
+++ server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,36 +19,25 @@ import com.najackdo.server.domain.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/book")
+@Slf4j
 public class BookController {
-    private final UserBooksService userBooksService;
+	private final UserBooksService userBooksService;
 
-//    @GetMapping("")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public SuccessResponse<List<UserBookData.Search>> index() {
-//        return SuccessResponse.of(userBooksService.getBooksByUserId("123"));
-//    }
+	@PostMapping("/registBooks")
+	public SuccessResponse<Void> registBooks(@CurrentUser User user, @RequestBody UserBookData.Create create) {
+		userBooksService.addBookList(user, create);
+		return SuccessResponse.empty();
+	}
 
-    @PostMapping("/registBooks")
-    public SuccessResponse<Void> registBooks(@CurrentUser User user, @RequestBody UserBookData.Create create) {
-        userBooksService.addBookList(user,create);
-        return SuccessResponse.empty();
-    }
-
-    @PostMapping("/registBook")
-    public SuccessResponse<Void> registBooks(@CurrentUser User user, @RequestBody UserBookData.CreateByISBN create) {
-        userBooksService.addBook(user,create);
-        return SuccessResponse.empty();
-    }
+	@PostMapping("/registBook")
+	public SuccessResponse<Void> registBooks(@CurrentUser User user, @RequestBody UserBookData.CreateByISBN create) {
+		userBooksService.addBook(user, create);
+		return SuccessResponse.empty();
+	}
 
 	private final BookService bookService;
 
@@ -75,15 +66,9 @@ public class BookController {
 		return SuccessResponse.of(bookService.getBookCaseByNickName(nickname));
 	}
 
+	@GetMapping("/book/interest")
+	public SuccessResponse<List<UserBookData.BookCase>> getBookCase(@CurrentUser User user) {
+		return SuccessResponse.of(userBooksService.getBookCasesByUserId(user));
+	}
 
-    @GetMapping("/bookcase/interest")
-    public SuccessResponse<List<UserBookData.BookCase>> getBookCase(@CurrentUser User user) {
-        return SuccessResponse.of(userBooksService.getBookCasesByUserId(user));
-    }
-
-    @GetMapping("/bookcase/{username}")
-    public SuccessResponse<List<UserBookData.BookCase>> getBookCase(@PathVariable String username) {
-        log.info(username);
-        return SuccessResponse.of(userBooksService.getBookCasesByUserName(username));
-    }
 }
