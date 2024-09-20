3204c5951ea11babeb35da5cf730d394aa78b8df
김도영 <rlaehdud1002@naver.com>
2024-09-20 10:25:49 +0900

    [FEAT] 자동완성 검색어

--- client/src/page/search/components/PopularSearch.tsx
+++ client/src/page/search/components/PopularSearch.tsx
@@ -3,7 +3,7 @@ import PopularSearchText from "page/search/components/PopularSearchText";
 const PopularSearch = () => {
   return (
     <div className="my-4">
-      <span className="font-bold">추천 검색</span>
+      <span className="font-bold">인기 검색</span>
       <div
         className="flex overflow-x-auto whitespace-nowrap space-x-3 mt-2"
         style={{
