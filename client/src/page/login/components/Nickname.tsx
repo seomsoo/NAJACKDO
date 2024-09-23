7499dedb35b627636537f921a53b20319d589c2c
김도영 <rlaehdud1002@naver.com>
2024-09-23 14:12:53 +0900

    [FEAT] valid state에 따른 redirect 로직 작성

--- client/src/page/login/components/Nickname.tsx
+++ client/src/page/login/components/Nickname.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { useQuery } from "react-query";
 import { availableNickname, getUserNickname } from "api/profileApi";
 import { Button } from "components/ui/button";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useQuery } from "react-query";
 import useSurveyStore from "store/useSurveyStore";
 
 const Nickname = () => {
-  const { nickname, setNickname, avaliableNickname, setAvaliableNickname } = useSurveyStore();
+  const { nickname, setNickname, avaliableNickname, setAvaliableNickname } =
+    useSurveyStore();
   const { register, handleSubmit, setValue } = useForm({
     defaultValues: { nickname: nickname },
   });
@@ -50,6 +51,10 @@ const Nickname = () => {
     setNicknameToCheck(data.nickname);
   };
 
+  if (userNicknameLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex flex-col items-center text-2xl py-8 font-semibold">
       <header>닉네임을 입력해 주세요.</header>
@@ -58,14 +63,20 @@ const Nickname = () => {
         <span>공백 없이 8자 이하, 기호는 -_. 만 사용 가능합니다.</span>
         <span>닉네임은 중복 불가입니다.</span>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)} className="mt-16 flex flex-col items-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mt-16 flex flex-col items-center"
+      >
         <input
           type="text"
           className="text-5xl text-center bg-transparent focus:outline-none focus:border-none placeholder:text-[#B0A695] text-[#B0A695]"
           {...register("nickname")}
           placeholder="닉네임"
         />
-        <Button type="submit" className="mt-8 bg-[#79AC78] hover:bg-[#a6b37d] text-white">
+        <Button
+          type="submit"
+          className="mt-8 bg-[#79AC78] hover:bg-[#a6b37d] text-white"
+        >
           중복 확인
         </Button>
       </form>
@@ -75,7 +86,9 @@ const Nickname = () => {
           {avaliableNickname ? (
             <span className="text-green-500">사용 가능한 닉네임입니다.</span>
           ) : (
-            <span className="text-red-500">닉네임이 중복되어 사용 불가합니다.</span>
+            <span className="text-red-500">
+              닉네임이 중복되어 사용 불가합니다.
+            </span>
           )}
         </div>
       )}
