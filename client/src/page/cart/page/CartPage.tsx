c09c558bf69406ff2c5a08ab069446e793ec2444
Chung <souffle1903@gmail.com>
2024-09-24 16:06:32 +0900

    [FEAT] 장바구니 api 연결중 close #46

--- client/src/page/cart/page/CartPage.tsx
+++ client/src/page/cart/page/CartPage.tsx
@@ -1,86 +1,155 @@
-import { IoIosArrowBack } from 'react-icons/io';
-import { useNavigate } from 'react-router-dom';
-import CartContainer from '../components/CartContainer';
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { ILocationRange } from "atoms/Location.type";
+import { getLocationRange, postMyLocation } from "api/locationApi";
+import { IoIosArrowBack } from "react-icons/io";
+import { RangeSlider } from "components/ui/rangeslider";
 
-const CartPage = () => {
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+const RangeSettingPage = () => {
+  const location = useLocation();
   const navigate = useNavigate();
-  const goBack = () => {
-    navigate(-1);
-  };
+  const { latitude, longitude, locationName, locationCode } = location.state || {};
+  const [map, setMap] = useState<any>(null);
+  const [mapLoaded, setMapLoaded] = useState(false);
+  const [ range, setRange ] = useState(0);
 
-  const cartArray = [
-    {
-      name: '서민수',
-      period: 14,
-      books: [
-        {
-          title: '이기적 유전자',
-          author: '리처드 도킨스',
-          price: 100,
-          status: '상',
-          image: 'https://placehold.co/41x60',
-        },
-        {
-          title: '이기적 유전자',
-          author: '리처드 도킨스',
-          price: 50,
-          status: '하',
-          image: 'https://placehold.co/41x60',
-        },
-      ],
-    },
-    {
-      name: '정하림',
-      period: 30,
-      books: [
-        {
-          title: '이기적 유전자',
-          author: '리처드 도킨스',
-          price: 100,
-          status: '상',
-          image: 'https://placehold.co/41x60',
-        },
-        {
-          title: '이기적 유전자',
-          author: '리처드 도킨스',
-          price: 100,
-          status: '상',
-          image: 'https://placehold.co/41x60',
-        },
-        {
-          title: '이기적 유전자 ',
-          author: '정하림',
-          price: 1000,
-          status: '상',
-          image: 'https://placehold.co/41x60',
-        },
-      ],
-    },
-  ];
+  const {
+    data: locationRange,
+    isLoading,
+    isError,
+  } = useQuery<ILocationRange[]>({
+    queryKey: ["locationRange"],
+    queryFn: () => getLocationRange(latitude, longitude),
+  });
+  console.log("locationRange", locationRange);
+  console.log("제발", locationRange?.[range]);
+
+  useEffect(() => {
+    if (!locationRange || !map) {
+      console.log("불러오는 중");
+      return;
+    }
+    console.log("불러옴");
+
+    // 폴리곤을 그리기 위한 함수 호출
+    displayPolygons(locationRange?.[range]);
+
+    function displayPolygons(locationRanges:any) {
+      locationRanges.forEach((location) => {
+        console.log("으아악 포이치", location);
+        const polygonCoordinates = parsePolygon(location.polygon);
+        if (polygonCoordinates.length > 0) {
+          const polygonPath = polygonCoordinates.map(
+            (coord) => new window.kakao.maps.LatLng(coord[0], coord[1])
+          );
+
+          const polygon = new window.kakao.maps.Polygon({
+            path: polygonPath,
+            strokeWeight: 2.5,
+            strokeColor: "#E8B900",
+            strokeOpacity: 1,
+            strokeStyle: "solid",
+            fillColor: "#E8B900",
+            fillOpacity: 0.4,
+          });
+
+          polygon.setMap(map);
+        }
+      });
+    }
+
+    function parsePolygon(polygon: string) {
+      if (!polygon) return [];
+      const polygonArray = polygon
+        .replace("MULTIPOLYGON (((", "")
+        .replace(")))", "")
+        .split(", ")
+        .map((latlng) => latlng.split(" ").map(Number));
+
+      console.log("polygonArray", polygonArray);
+      return polygonArray;
+    }
+  }, [locationRange, map]);
+
+  useEffect(() => {
+    const mapContainer = document.getElementById("kakaomap");
+    if (!mapContainer) {
+      setTimeout(() => setMapLoaded(mapLoaded => !mapLoaded), 1000);
+      return;
+    }
+    const mapOption = {
+      center: new window.kakao.maps.LatLng(longitude, latitude),
+      level: 7,
+    };
+    const mapInstance = new window.kakao.maps.Map(mapContainer, mapOption);
+    setMap(mapInstance);
+  }, [latitude, longitude, mapLoaded, range]);
+
+  const mutation = useMutation({
+    mutationKey: ["location"],
+    mutationFn: postMyLocation,
+    
+    onSuccess: () => {
+      alert("오예 성공")
+      navigate('/')
+    }
+  })
+
+  const handleLocationClick = () => {
+    mutation.mutate({
+      locationCode: locationCode,
+      distanceMeters: (range * 0.005 + 0.02)
+    })
+    console.log("locationCode", locationCode,"distanceMeters", range * 0.005 + 0.02)
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading location data.</div>;
+  }
 
   return (
-    <div className='mx-6 py-4'>
-      <div className='flex flex-row gap-1'>
-        <button onClick={goBack}>
-          <IoIosArrowBack className='text-xl' />
+    <div>
+      <div className="flex flex-row mx-6 py-4">
+        <button onClick={() => navigate(-1)}>
+          <IoIosArrowBack />
         </button>
-        <p className='text-2xl font-bold '>장바구니</p>
+        <p className="text-2xl font-bold ml-2">범위 설정</p>
       </div>
-
-      <div>
-        {cartArray.map((item, index) => {
-          return (
-            <CartContainer
-              key={index}
-              name={item.name}
-              period={item.period}
-              books={item.books}
-            />
-          );
-        })}
+      <div id="kakaomap" className="w-full h-[400px]"></div>
+      <div className="mx-6 my-10 flex flex-row justify-center items-center">
+        <p>가까운</p>
+          <RangeSlider
+          min={0}
+          max={locationRange?.length - 1 || 0} //3
+          step={1}
+          value={[range]}  
+          onValueChange={(value) => setRange(value[0])}
+          className="w-[240px]"
+          />
+        <p>먼</p>
+      </div>
+      <div className="flex justify-center mt-3">
+        <button 
+          className="w-[265px] bg-[#776B5D] text-white rounded-[12px] p-2"
+          onClick={handleLocationClick}
+        >
+          <span className="font-bold">{locationName.split(' ').slice(-1)[0]}</span>으로 지역 설정
+        </button>
       </div>
     </div>
   );
 };
 
-export default CartPage;
+export default RangeSettingPage;
