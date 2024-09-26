2d28ea4fe58aa061fd3ea3eda4af74d4d43b260d
Chung <souffle1903@gmail.com>
2024-09-26 14:48:55 +0900

    [FEAT] 도서 상세 조회 API 연결

--- client/src/page/library/components/BookInfo.tsx
+++ client/src/page/library/components/BookInfo.tsx
@@ -3,39 +3,55 @@ import { IoHeartOutline } from "react-icons/io5";
 
 interface BookInfoProps {
   book: {
+    bookId: number;
     title: string;
-    author: string[];
+    author: string;
+    cover: string;
     genre: string;
-    category: string[];
-    content: string;
-    price: number;
+    description: string;
+    publisher: string;
+    priceStandard: number;
+    itemPage: number;
+    starPoint: number;
+    pubDate: string;
+    isbn: number;
   };
   rental?: boolean;
 }
-
+  // const bookData = {
+  //   title: "안녕, 푸바오",
+  //   author: ["장린 지음"],
+  //   genre: "에세이",
+  //   category: ["동물 에세이", "포토 에세이"],
+  //   content:
+  //     "우리가 낳아 키운 푸바오는 한국을 떠나 잘 지내고 있을까? <br/> 5개월간 우리 곁을 떠났던 푸바오, <br/> 기나긴 그리움을 뚫고 포토에세이 《안녕, 푸바오》로 돌아왔다.",
+  //   price: 15000,
+  // };
 const BookInfo = ({ book, rental }: BookInfoProps) => {
+  const authorList = book.author.replace(" (지은이)", "").split(", ");
+  const author = authorList.length > 1 ? authorList[0]+" 외 "+ (authorList.length-1) + "명" : authorList[0]
+  const genreList = book.genre.split(">");
+  const genre = genreList.length > 2 ? genreList.slice(1, 3) : genreList;
+  console.log("genre", genre);
+  console.log("genreList", genreList);
+  
   return (
     <div>
       <div className="flex flex-row justify-between items-center">
         <p className="text-xl font-bold">{book.title}</p>
         {!rental && <IoHeartOutline size={25} />}
       </div>
-      {book.author.map((author, index) => {
-        return (
-          <p key={index} className="my-2">
-            {author}
-          </p>
-        );
-      })}
-      {book.category.map((category, index) => {
-        return <CategoryTag key={index} category={category} />;
+      <p>{author} 지음</p>
+
+      {genre.map((genre, index) => {
+        return <CategoryTag key={index} category={genre} />;
       })}
       <p
-        dangerouslySetInnerHTML={{ __html: book.content }}
+        dangerouslySetInnerHTML={{ __html: book.description }}
         className="my-8"
       ></p>
       {/* <p>{book.content}</p> */}
-      <p className="font-bold">중고가 : {book.price}</p>
+      <p className="font-bold">중고가 : {book.priceStandard}</p>
     </div>
   );
 };
--- client/src/page/library/page/BookDetailPage.tsx
+++ client/src/page/library/page/BookDetailPage.tsx
@@ -1,27 +1,58 @@
+import { getBookDetail } from "api/bookApi";
 import BookInfo from "page/library/components/BookInfo";
 import CenterCropImage from "page/library/components/CenterCropImage";
 import DetailRecommendBook from "page/library/components/DetailRecommendBook";
 import RentableBook from "page/library/components/RentableBook";
 import { IoChevronBack } from "react-icons/io5";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useQuery } from '@tanstack/react-query';
+
 
 const BookDetailPage = () => {
+  const { bookId } = useParams();
   const navigate = useNavigate();
+  const bookIdAsNumber = parseInt(bookId, 10);
+  console.log("bookIdAsNumber", bookIdAsNumber);
 
-  const bookData = {
-    title: "안녕, 푸바오",
-    author: ["장린 지음"],
-    genre: "에세이",
-    category: ["동물 에세이", "포토 에세이"],
-    content:
-      "우리가 낳아 키운 푸바오는 한국을 떠나 잘 지내고 있을까? <br/> 5개월간 우리 곁을 떠났던 푸바오, <br/> 기나긴 그리움을 뚫고 포토에세이 《안녕, 푸바오》로 돌아왔다.",
-    price: 15000,
-  };
+
+  // 다른 사용자의 책장 정보 조회
+  const {
+    data: bookData,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['bookdetail', bookIdAsNumber],
+    queryFn: () => getBookDetail(bookIdAsNumber),
+  });
+  console.log("bookData", bookData);
+  console.log("지은이", );
+
+
+  if (isLoading) {
+    return <div>로딩 중...</div>;
+  }
+
+  if (isError) {
+    return <div>에러가 발생했습니다.</div>;
+  }
+
+
+
+
+  // const bookData = {
+  //   title: "안녕, 푸바오",
+  //   author: ["장린 지음"],
+  //   genre: "에세이",
+  //   category: ["동물 에세이", "포토 에세이"],
+  //   content:
+  //     "우리가 낳아 키운 푸바오는 한국을 떠나 잘 지내고 있을까? <br/> 5개월간 우리 곁을 떠났던 푸바오, <br/> 기나긴 그리움을 뚫고 포토에세이 《안녕, 푸바오》로 돌아왔다.",
+  //   price: 15000,
+  // };
 
   return (
     <div>
       <div className="relative w-full h-72 object-cover">
-        <CenterCropImage imageUrl="/pubao.png" />
+        <CenterCropImage imageUrl={bookData.cover} />
         <div
           onClick={() => navigate(-1)}
           className="cursor-pointer absolute left-0 top-0 z-10 p-4"
@@ -29,7 +60,7 @@ const BookDetailPage = () => {
           <IoChevronBack size={25} color="#FFFFFF" />
         </div>
         <div className="absolute inset-0 flex items-center justify-center">
-          <img src="/pubao.png" alt="푸바오" width={180} className="z-20" />
+          <img src={bookData.cover} alt="사진 커버" width={180} className="z-20" />
         </div>
       </div>
       <div className="m-4">
