4a423d7ee79f2c7999aa946c706afb8aba31d8bb
ParkBeomSu <beomsu4639@gmail.com>
2024-09-27 11:02:01 +0900

    [FEAT] 기존 알람 조회 변경 및 알람 읽음 처리 API 추가

--- server/src/main/java/com/najackdo/server/domain/notification/repository/NotificationRepository.java
+++ server/src/main/java/com/najackdo/server/domain/notification/repository/NotificationRepository.java
@@ -3,7 +3,12 @@ package com.najackdo.server.domain.notification.repository;
 import com.najackdo.server.domain.notification.entity.Notification;
 import com.najackdo.server.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>, NotificationQueryRepository {
 
+    @Modifying
+    @Query("update Notification n set n.isRead = true where n.isSend = true")
+    int  updateNotificationIsRead();
 }
