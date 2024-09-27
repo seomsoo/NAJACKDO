12d00275cf8a2fc2baeea0800c07cec6487fb2dc
김도영 <rlaehdud1002@naver.com>
2024-09-27 15:31:45 +0900

    [FEAT] 채팅 충

--- client/src/page/chatting/components/ChatBookInfo.tsx
+++ client/src/page/chatting/components/ChatBookInfo.tsx
@@ -1,4 +1,7 @@
+import RentalModal from "page/chatting/components/RentalModal";
+import { useState } from "react";
 import { IoIosLeaf } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 interface ChatBookInfoProps {
   book: {
@@ -8,7 +11,40 @@ interface ChatBookInfoProps {
   };
 }
 
+enum ChatRentalStep {
+  PAY,
+  RETURN,
+  REVIEW,
+}
+
 const ChatBookInfo = ({ book }: ChatBookInfoProps) => {
+  const navigate = useNavigate();
+  const [step, setStep] = useState<ChatRentalStep>(ChatRentalStep.REVIEW);
+  let rentalStep;
+  const isOwner = false;
+
+  switch (step) {
+    case 0:
+      rentalStep = "송금하기";
+      break;
+
+    case 1:
+      rentalStep = "반납하기";
+      break;
+
+    case 2:
+      rentalStep = "후기 보내기";
+      break;
+
+    default:
+      rentalStep = "송금하기";
+      break;
+  }
+
+  const handleReview = () => {
+    navigate("/chat/review");
+  };
+
   return (
     <div className="bg-[#DBD6D3] w-full h-24 px-4 flex flex-row items-center justify-between">
       <div className="flex flex-row items-center">
@@ -22,9 +58,16 @@ const ChatBookInfo = ({ book }: ChatBookInfoProps) => {
           </div>
         </div>
       </div>
-      <span className="bg-[#776B5D] text-white rounded-lg py-2 px-3">
-        약속 잡기
-      </span>
+      {step !== 2 ? (
+        <RentalModal step={step} rentalStep={rentalStep} isOwner={isOwner} />
+      ) : (
+        <button
+          className="bg-[#776B5D] text-white rounded-lg py-2 px-3"
+          onClick={handleReview}
+        >
+          {rentalStep}
+        </button>
+      )}
     </div>
   );
 };
---
+++ client/src/page/chatting/components/RentalModal.tsx
@@ -0,0 +1,63 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTrigger,
+} from "components/ui/dialog";
+import { IoLibrary } from "react-icons/io5";
+import { MdLibraryAddCheck } from "react-icons/md";
+
+interface RentalModalProps {
+  step: number;
+  rentalStep: number;
+  isOwner: boolean;
+}
+
+const RentalModal = ({ step, rentalStep, isOwner }: RentalModalProps) => {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <span className="bg-[#776B5D] text-white rounded-lg py-3 px-4">
+          {rentalStep}
+        </span>
+      </DialogTrigger>
+      <DialogContent className="bg-[#F1ECE3] rounded-2xl">
+        <DialogDescription className="flex flex-col items-center space-y-7 text-center text-black text-bol">
+          {step === 0 && (
+            <>
+              <IoLibrary size={35} color="#5F6F52" />
+              <span className="text-lg">
+                정하림님에게 5,000 책잎을 <br />
+                송금하시겠습니까?
+              </span>
+              <div className="space-x-6">
+                <button className="border-2 border-[#776B5D] px-10 py-2 rounded-xl hover:bg-[#EBE9E7]">
+                  취소
+                </button>
+                <button className="border-2 border-[#776B5D] px-10 py-2 rounded-xl bg-[#776B5D] hover:bg-[#6F473D] text-white">
+                  확인
+                </button>
+              </div>
+            </>
+          )}
+          {step === 1 && (
+            <>
+              <MdLibraryAddCheck size={35} color="#776B5D" />
+              <span className="text-lg">반납을 확인하시겠습니까?</span>
+              <div className="space-x-6">
+                <button className="border-2 border-[#776B5D] px-10 py-2 rounded-xl hover:bg-[#EBE9E7]">
+                  취소
+                </button>
+                <button className="border-2 border-[#776B5D] px-10 py-2 rounded-xl bg-[#776B5D] hover:bg-[#4F473D] text-white">
+                  확인
+                </button>
+              </div>
+            </>
+          )}
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default RentalModal;
