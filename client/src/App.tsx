ef74138f312efd5168cbbcc584ccedff218d5033
서민수 <minsu9812@naver.com>
2024-09-27 14:46:45 +0900

    [FEAT] 설문조사페이지 풋터 제거

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,17 @@
 // src/App.tsx
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { IValid } from "atoms/Valid.type";
-import Footer from "components/common/Footer";
-import Header from "components/common/Header";
-import MainRoute from "components/routes/MainRoute";
-import { useState } from "react";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
-import { useAuthStore } from "store/useAuthStore";
-import { useValidStore } from "store/useValidStore";
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { IValid } from 'atoms/Valid.type';
+import Footer from 'components/common/Footer';
+import Header from 'components/common/Header';
+import MainRoute from 'components/routes/MainRoute';
+import { useState } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { useAuthStore } from 'store/useAuthStore';
+import { useValidStore } from 'store/useValidStore';
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 function App() {
-
   const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -21,9 +19,9 @@ function App() {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   };
-  
+
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   // Initialize Firebase Cloud Messaging and get a reference to the service
@@ -35,11 +33,13 @@ function App() {
     try {
       // Request permission for notifications
       const permission = await Notification.requestPermission();
-      
+
       if (permission === 'granted') {
         console.log('Notification permission granted.');
         // Get the FCM token
-        const token = await getToken(messaging, {vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY})
+        const token = await getToken(messaging, {
+          vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY,
+        });
         console.log('FCM Token:', token);
       } else {
         console.log('Notification permission denied.');
@@ -50,20 +50,25 @@ function App() {
         console.log('app.tsx');
         // Handle the notification or update your UI
       });
-      
     } catch (error) {
       console.error('Error setting up notifications:', error);
     }
-  };                
+  };
 
   const navigate = useNavigate();
   const location = useLocation();
   const currentPath = location.pathname;
   const queryClient = new QueryClient();
 
-  const popupPaths = ["/kapay/approve", "/kapay/cancel", "/kapay/fail"];
-  const showHeaderPaths = ["/"];
-  const hideFooterPaths = ["/sign-in", "/book/:bookId/rental", "/book/:bookId/mybook"];
+  const popupPaths = ['/kapay/approve', '/kapay/cancel', '/kapay/fail'];
+  const showHeaderPaths = ['/'];
+  const hideFooterPaths = [
+    '/sign-in',
+    '/book/:bookId/rental',
+    '/book/:bookId/mybook',
+    '/survey',
+    '/setting/location',
+  ];
 
   const { accessToken } = useAuthStore.getState();
   const { isSurvey, isLocation, setIsSurvey, setIsLocation } =
@@ -133,12 +138,12 @@ function App() {
   const shouldHideHeaderFooter = popupPaths.includes(currentPath) && isPopup;
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="pb-[86px] relative">
+      <div className='pb-[86px] relative'>
         {!shouldHideHeaderFooter && showHeaderPaths.includes(currentPath) && (
           <Header />
         )}
         <Routes>
-          <Route path="/*" element={<MainRoute />} />
+          <Route path='/*' element={<MainRoute />} />
         </Routes>
         {!shouldHideHeaderFooter && !hideFooterPaths.includes(currentPath) && (
           <Footer />
