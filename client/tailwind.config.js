1d1453b526fd1f75eaa0ebf0c251fd4145c69fd6
lij0825 <dldlswns890@gmail.com>
2024-09-29 21:50:55 +0900

    [FIX] 누군가는 해야하는 의존성 정리 및 파일 정리

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
+  		center: true,
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -95,22 +95,6 @@ module.exports = {
   					height: '0'
   				}
   			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
   		},
   		animation: {
 				glow: 'glow 2s ease-in-out infinite',
@@ -119,12 +103,10 @@ module.exports = {
         slideUp: 'slideUp 1.5s ease-in-out',
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate"),
-		require('@tailwindcss/line-clamp'),
+	plugins: [
+		require("tailwindcss-animate")
 	],
 }
