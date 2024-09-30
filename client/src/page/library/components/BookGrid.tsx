f8342108ff183c1eff537280d5e4f466b981b0fe
서민수 <minsu9812@naver.com>
2024-09-30 18:04:30 +0900

    [FEAT] Ai 사진 체크 진행중

--- client/src/page/library/components/BookGrid.tsx
+++ client/src/page/library/components/BookGrid.tsx
@@ -1,5 +1,6 @@
 import { FaCheck } from 'react-icons/fa';
 import { IBookCase } from 'atoms/BookCase.type';
+import { useNavigate } from 'react-router-dom'; // useNavigate 추가
 
 type BookGridProps = {
   books: IBookCase['displayBooks'];
@@ -9,6 +10,8 @@ type BookGridProps = {
 };
 
 const BookGrid = ({ books, isMyBookCase, checked, onCheck }: BookGridProps) => {
+  const navigate = useNavigate(); // navigate 선언
+
   // 이미지를 3개씩 묶는 함수
   const chunkArray = (
     arr: IBookCase['displayBooks'],
@@ -26,35 +29,35 @@ const BookGrid = ({ books, isMyBookCase, checked, onCheck }: BookGridProps) => {
   // 책 상태에 따른 뱃지 렌더링 함수
   const renderBadge = (bookStatus: string) => {
     if (isMyBookCase) {
-      if (bookStatus === '대여 중') {
+      if (bookStatus === 'RENTED') {
         return (
-          <span className='absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#5f6f52] p-2 px-3 rounded-2xl text-white font-light text-xs text-nowrap mb-1'>
+          <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#5f6f52] p-2 px-3 rounded-2xl text-white font-light text-xs text-nowrap mb-1">
             대여 중
           </span>
         );
-      } else if (bookStatus === '연체 중') {
+      } else if (bookStatus === 'UNAVAILABLE') {
         return (
-          <span className='absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#D96363] p-2 px-3 rounded-2xl text-white font-light text-xs text-nowrap mb-1'>
-            연체 중
+          <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#D96363] p-2 px-3 rounded-2xl text-white font-light text-xs text-nowrap mb-1">
+            대여 불가
           </span>
         );
-      } else if (bookStatus === '인증 필요') {
+      } else if (bookStatus === 'NOT_INSPECTED') {
         return (
-          <span className='absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#3078E4] p-2 px-3 rounded-2xl text-white font-light text-xs text-nowrap mb-1'>
+          <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#3078E4] p-2 px-3 rounded-2xl text-white font-light text-xs text-nowrap mb-1">
             인증 필요
           </span>
         );
       }
     } else {
-      if (bookStatus === '대여 가능') {
+      if (bookStatus === 'AVAILABLE') {
         return (
-          <span className='absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#5f6f52] p-2 px-3 rounded-2xl text-white font-light text-xs text-nowrap mb-1'>
+          <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#5f6f52] p-2 px-3 rounded-2xl text-white font-light text-xs text-nowrap mb-1">
             대여 가능
           </span>
         );
-      } else if (bookStatus === '대여 불가') {
+      } else if (bookStatus === 'UNAVAILABLE') {
         return (
-          <span className='absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#D96363] p-2 px-3 rounded-2xl text-white font-light text-xs text-nowrap mb-1'>
+          <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#D96363] p-2 px-3 rounded-2xl text-white font-light text-xs text-nowrap mb-1">
             대여 불가
           </span>
         );
@@ -63,19 +66,28 @@ const BookGrid = ({ books, isMyBookCase, checked, onCheck }: BookGridProps) => {
     return null;
   };
 
+  const handleBookClick = (book: IBookCase['displayBooks'][0]) => {
+    if (book.bookStatus === 'NOT_INSPECTED') {
+      // 인증 필요 상태일 경우에만 이동
+      navigate(`/ai-check`, { state: { userId: 1, userBookId: book.bookId } }); // userId와 bookId를 state로 전달
+    } else {
+      alert('인증이 필요하지 않은 책입니다.');
+    }
+  };
+
   return (
     <>
       {bookChunks.map((chunk, chunkIndex) => (
-        <div key={chunkIndex} className='mb-9'>
-          <div className='grid grid-cols-3 gap-4'>
+        <div key={chunkIndex} className="mb-9">
+          <div className="grid grid-cols-3 gap-4">
             {chunk.map((book, index) => (
-              <div key={book.bookId}>
+              <div key={book.bookId} onClick={() => handleBookClick(book)}>
                 {!isMyBookCase && onCheck && checked && (
-                  <div className='mb-1 ml-2'>
+                  <div className="mb-1 ml-2">
                     <input
-                      type='checkbox'
+                      type="checkbox"
                       id={`check-${chunkIndex * 3 + index}`} // 고유한 id 부여
-                      className='hidden peer'
+                      className="hidden peer"
                       checked={checked[chunkIndex * 3 + index]} // 각 이미지에 대한 체크 상태
                       onChange={() => onCheck(chunkIndex * 3 + index)} // 체크박스 상태 변경 핸들러
                     />
@@ -88,17 +100,17 @@ const BookGrid = ({ books, isMyBookCase, checked, onCheck }: BookGridProps) => {
                       }`}
                     >
                       {checked[chunkIndex * 3 + index] && (
-                        <FaCheck className='text-white w-4 h-4' />
+                        <FaCheck className="text-white w-4 h-4" />
                       )}
                     </label>
                   </div>
                 )}
 
-                <div className='flex flex-col items-center relative'>
+                <div className="flex flex-col items-center relative">
                   <img
                     src={book.cover}
                     alt={`book-${index}`}
-                    className='w-20 h-28 object-cover shadow-book-shadow rounded-r-lg rounded-br-lg'
+                    className="w-20 h-28 object-cover shadow-book-shadow rounded-r-lg rounded-br-lg"
                   />
                   {renderBadge(book.bookStatus)} {/* 책 상태 뱃지 표시 */}
                 </div>
@@ -106,7 +118,7 @@ const BookGrid = ({ books, isMyBookCase, checked, onCheck }: BookGridProps) => {
             ))}
           </div>
           <div>
-            <img src='/images/library/bar.png' alt='bar' className='w-full' />
+            <img src="/images/library/bar.png" alt="bar" className="w-full" />
           </div>
         </div>
       ))}
