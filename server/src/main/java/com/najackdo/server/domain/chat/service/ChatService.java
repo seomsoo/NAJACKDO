99de8906382dea80fdf1c192d53ed9a835e333a5
SeongMin Hwang <qq221qq@naver.com>
2024-10-04 15:54:34 +0900

    [FEAT] 채팅방 생성 시 카트 isDelete처리 수정

--- server/src/main/java/com/najackdo/server/domain/chat/service/ChatService.java
+++ server/src/main/java/com/najackdo/server/domain/chat/service/ChatService.java
@@ -71,8 +71,7 @@ public class ChatService {
 
 		User owner = userRepository.findById(ownerId).orElseThrow(() -> new BaseException(ErrorCode.NOT_FOUND_USER));
 		Cart cart = cartRepository.findById(cartId).orElseThrow(() -> new BaseException(ErrorCode.NOT_FOUND_CART));
-
-		cart.isDelete();
+		cart.deleteCart();
 		cartRepository.save(cart);
 
 		ChatRoom chatRoom = chatRoomRepository.save(
