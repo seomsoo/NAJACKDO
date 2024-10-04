852f38875c0d9928098f11522cdedf005bde8553
서민수 <minsu9812@naver.com>
2024-10-04 17:35:36 +0900

    [FEAT] 베스트셀러 연결완료결

--- client/src/atoms/Book.type.ts
+++ client/src/atoms/Book.type.ts
@@ -1,3 +1,19 @@
+export interface IBestSeller {
+  bookId: number;
+  title: string;
+  author: string;
+  cover: string;
+  genre: string;
+  description: string;
+  publisher: string;
+  priceStandard: number;
+  itemPage: number;
+  starPoint: number;
+  pubDate: string;
+  isbn: number;
+  interest: boolean;
+}
+
 export interface IBookDetail {
   bookId: number;
   title: string;
