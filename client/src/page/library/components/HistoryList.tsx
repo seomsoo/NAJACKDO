d8387a348d285f583c2b306bea5aa3ff2eca5ccf
김도영 <rlaehdud1002@naver.com>
2024-10-04 16:40:49 +0900

    [MERGE]

--- client/src/page/library/components/HistoryList.tsx
+++ client/src/page/library/components/HistoryList.tsx
@@ -8,6 +8,7 @@ import {
 
 interface IHistoryListProps {
   historyData?: IHistory[];
+  title: string;
 }
 
 export const formatDate = (dateString: string) => {
@@ -19,54 +20,57 @@ export const formatDate = (dateString: string) => {
   });
 };
 
-const HistoryList: React.FC<IHistoryListProps> = ({ historyData }) => {
+const HistoryList: React.FC<IHistoryListProps> = ({ historyData, title }) => {
   return (
     <div className="flex flex-col">
-
-      {(!historyData || historyData.length === 0) ? (
-        <div className="text-center text-gray-500 mt-[70%]">데이터가 없습니다.</div>
+      {!historyData || historyData.length === 0 ? (
+        <div className="text-center text-gray-500 mt-[70%]">
+          데이터가 없습니다.
+        </div>
       ) : (
-      <Accordion type="multiple">
-        {" "}
-        {historyData.map((history) => (
-          <AccordionItem
-            key={history.rentalId.toString()}
-            value={history.rentalId.toString()}
-          >
-            <AccordionTrigger className="flex items-center">
-              <img
-                src={history.otherUseProfileImg}
-                alt="Profile"
-                className="w-10 h-10 rounded-full mr-4"
-              />
-              <div className="flex flex-col">
-                <div className="font-semibold">{history.otherUseNickName}</div>
-                <div className="text-sm">
-                  {formatDate(history.rentalStartDate)} ~{" "}
-                  {formatDate(history.rentalEndDate)}
-                </div>
-              </div>
-            </AccordionTrigger>
-            <AccordionContent>
-              <div className="mt-2">
-                {history.rentalBooks.map((book) => (
-                  <div key={book.title} className="flex items-center mt-2">
-                    <img
-                      src={book.coverImg}
-                      alt="Book cover"
-                      className="w-20 h-28 object-cover mr-4"
-                    />
-                    <div>
-                      <div className="font-semibold">{book.title}</div>
-                      <div className="text-sm">{book.author}</div>
-                      <div className="text-sm">{book.publisher}</div>
-                    </div>
+        <Accordion type="multiple">
+          {" "}
+          {historyData.map((history) => (
+            <AccordionItem
+              key={history.rentalId.toString()}
+              value={history.rentalId.toString()}
+            >
+              <AccordionTrigger className="flex items-center">
+                <img
+                  src={history.otherUseProfileImg}
+                  alt="Profile"
+                  className="w-10 h-10 rounded-full mr-4"
+                />
+                <div className="flex flex-col">
+                  <div className="font-semibold">
+                    {history.otherUseNickName}
                   </div>
-                ))}
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        ))}
+                  <div className="text-sm">
+                    {formatDate(history.rentalStartDate)} ~{" "}
+                    {formatDate(history.rentalEndDate)}
+                  </div>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="mt-2">
+                  {history.rentalBooks.map((book) => (
+                    <div key={book.title} className="flex items-center mt-2">
+                      <img
+                        src={book.coverImg}
+                        alt="Book cover"
+                        className="w-20 h-28 object-cover mr-4"
+                      />
+                      <div>
+                        <div className="font-semibold">{book.title}</div>
+                        <div className="text-sm">{book.author}</div>
+                        <div className="text-sm">{book.publisher}</div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
         </Accordion>
       )}
     </div>
