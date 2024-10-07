da29b6dd969094c24ac462e576b71d379b7fca47
김도영 <rlaehdud1002@naver.com>
2024-10-07 13:42:51 +0900

    [FEAT] Error Boundary 적용

--- client/src/page/chatting/page/ChattingPage.tsx
+++ client/src/page/chatting/page/ChattingPage.tsx
@@ -1,14 +1,18 @@
 import Loading from "components/common/Loading";
+import SmallError from "components/common/SmallError";
 import ChatRoomList from "page/chatting/components/ChatRoomList";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 const ChattingPage = () => {
   return (
-    <Suspense fallback={<Loading />}>
-      <div className="mx-[25px] relative">
-        <ChatRoomList />
-      </div>
-    </Suspense>
+    <ErrorBoundary fallback={<SmallError />}>
+      <Suspense fallback={<Loading />}>
+        <div className="mx-[25px] relative">
+          <ChatRoomList />
+        </div>
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
