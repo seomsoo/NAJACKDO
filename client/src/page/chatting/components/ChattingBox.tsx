1e9bc3b53d281f7b0c7635dd74d22ec158e75bda
Chung <souffle1903@gmail.com>
2024-10-08 14:37:53 +0900

    [FIX] 채팅방 수정 키보드

--- client/src/page/chatting/components/ChattingBox.tsx
+++ client/src/page/chatting/components/ChattingBox.tsx
@@ -105,7 +105,21 @@ const ChattingBox = ({
     }
   }, [messages]);
 
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.visualViewport) {
+        const isOpen = window.visualViewport.height < window.innerHeight;
+        setIsKeyboardOpen(isOpen);
+      }
+    };
+
+    window.visualViewport?.addEventListener("resize", handleResize);
+    return () => {
+      window.visualViewport?.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="mx-[25px]">
@@ -147,7 +161,13 @@ const ChattingBox = ({
         </div>
         <div ref={messagesEndRef} />
       </div>
-      <form onSubmit={handleSendMessage}>
+      <form onSubmit={handleSendMessage}
+      style={{
+        position: 'fixed',
+        zIndex: -1,
+        bottom: isKeyboardOpen ? 86 : 0,
+        transition: 'bottom 0.3s ease-in-out',
+      }}>
         <div className="flex flex-row items-center my-4">
           <Input
             placeholder="메시지를 입력해주세요."
