3d20d83d56240a9e5feda0b20ae17de4bab11fae
김도영 <rlaehdud1002@naver.com>
2024-10-08 15:10:33 +0900

    [FIX] infinity query일 때 inner scroll 해결

--- client/src/page/bookapply/page/BookApplyPage.tsx
+++ client/src/page/bookapply/page/BookApplyPage.tsx
@@ -1,10 +1,11 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
 import { getBookInfo, postRegisterBook } from "api/bookApi";
 import AlertModal from "components/common/AlertModal";
 import Error from "components/common/Error";
 import Loading from "components/common/Loading";
 import ApplyBookInfo from "page/bookapply/components/ApplyBookInfo";
-import { useState } from "react";
+import { Suspense, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { HiOutlineCamera } from "react-icons/hi2";
 import { IoChevronBack } from "react-icons/io5";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -15,11 +16,7 @@ const BookApplyPage = () => {
   const { kind, keyword } = location.state;
   const [open, setOpen] = useState<boolean>(false);
 
-  const {
-    data: bookInfo,
-    isLoading: bookInfoLoading,
-    isError,
-  } = useQuery({
+  const { data: bookInfo } = useSuspenseQuery({
     queryKey: ["book", "apply"],
     queryFn: () => getBookInfo({ kind, keyword }),
   });
@@ -38,75 +35,66 @@ const BookApplyPage = () => {
   });
 
   const handleApply = () => {
-    console.log("isbn", bookInfo.isbn);
     if (bookInfo.isbn) {
       mutation.mutate(bookInfo.isbn);
     }
   };
 
-  if (bookInfoLoading) {
-    return <Loading />;
-  }
-
-  if (isError) {
-    return <Error />;
-  }
-
-  if (bookInfo) {
-    console.log("bookInfo", bookInfo);
-  }
-
   return (
-    <div>
-      <div
-        onClick={() => navigate(-1)}
-        className="cursor-pointer p-4 flex flex-row items-center"
-      >
-        <IoChevronBack size={25} color="#545454" />
-        <span className="font-bold text-xl ml-2">
-          도서 등록 - 도서 단일 촬영
-        </span>
-      </div>
-      <div className="px-[25px]">
-        <div className="flex flex-col items-center">
-          <span className="font-bold text-base mb-4">도서 표지</span>
-          <img
-            src={bookInfo.cover}
-            alt={bookInfo.title}
-            width={200}
-            height={270}
-          />
+    <ErrorBoundary fallback={<Error />}>
+      <Suspense fallback={<Loading />}>
+        <div
+          onClick={() => navigate(-1)}
+          className="cursor-pointer p-4 flex flex-row items-center"
+        >
+          <IoChevronBack size={25} color="#545454" />
+          <span className="font-bold text-xl ml-2">
+            도서 등록 - 도서 단일 촬영
+          </span>
         </div>
-        <div>
-          <p className="font-bold text-base text-center my-4">등록 도서 정보</p>
-          <ApplyBookInfo book={bookInfo} />
-        </div>
-        <div className="flex flex-row justify-center space-x-4 my-8">
-          <div
-            className="flex flex-row justify-center items-center bg-sub1 px-5 py-2 rounded-lg cursor-pointer"
-            onClick={() => navigate("/apply/isbn")}
-          >
-            <span className="text-white font-bold text-sm">
-              도서 다시 촬영하기
-            </span>
-            <HiOutlineCamera color="white" className="ml-1" />
+        <div className="px-[25px]">
+          <div className="flex flex-col items-center">
+            <span className="font-bold text-base mb-4">도서 표지</span>
+            <img
+              src={bookInfo.cover}
+              alt={bookInfo.title}
+              width={200}
+              height={270}
+            />
           </div>
-          <button
-            className="bg-sub7 text-white text-sm font-bold rounded-lg px-8 py-3"
-            onClick={handleApply}
-          >
-            등록하기(총 1권)
-          </button>
+          <div>
+            <p className="font-bold text-base text-center my-4">
+              등록 도서 정보
+            </p>
+            <ApplyBookInfo book={bookInfo} />
+          </div>
+          <div className="flex flex-row justify-center space-x-4 my-8">
+            <div
+              className="flex flex-row justify-center items-center bg-sub1 px-5 py-2 rounded-lg cursor-pointer"
+              onClick={() => navigate("/apply/isbn")}
+            >
+              <span className="text-white font-bold text-sm">
+                도서 다시 촬영하기
+              </span>
+              <HiOutlineCamera color="white" className="ml-1" />
+            </div>
+            <button
+              className="bg-sub7 text-white text-sm font-bold rounded-lg px-8 py-3"
+              onClick={handleApply}
+            >
+              등록하기(총 1권)
+            </button>
+          </div>
         </div>
-      </div>
-      {open && (
-        <AlertModal
-          open={open}
-          setOpen={setOpen}
-          content="이미 등록된 도서입니다."
-        />
-      )}
-    </div>
+        {open && (
+          <AlertModal
+            open={open}
+            setOpen={setOpen}
+            content="이미 등록된 도서입니다."
+          />
+        )}
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
